748b730476a73aa9b3c75f778c91cb46ac04f015
daniels600 <59796882+daniels600@users.noreply.github.com>
2020-03-09 01:24:10 +0000

    Updated Covid class

--- CF_Challenge/src/Challenge/Covid.java
+++ CF_Challenge/src/Challenge/Covid.java
@@ -1,5 +1,4 @@
-package Challenge;
-
+package Career_Fair_Challenge;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,10 +12,25 @@ public class Covid {
     private String Geold;
     private int newconfDeaths;
     private int newConfCases;
+    private String Highest_name;
     private int highest = 0;
 
 
+
     public Covid() {
+    }
+
+    /**
+    public String getHighest_name() {
+        return Highest_name;
+    }
+
+    public void setHighest_name(String highest_name) {
+        Highest_name = highest_name;
+    }
+*/
+    public Covid(String Highest_name){
+        this.Highest_name = Highest_name;
     }
 
     public Covid(String dateRep, String country, String geold, int newconfDeaths, int newConfCases) {
@@ -67,40 +81,23 @@ public class Covid {
     public void setNewConfCases(int newConfCases) {
         this.newConfCases = newConfCases;
     }
-    /**
-    public int getHighestInfestion() {
-        return highest;
-    }
-*/
-
-
-    public void add_data(MultiMap<String, Covid> multiMap) {
-        Covid covid = new Covid();
-        MultiMap<String, Covid> multimap = new MultiMap<String, Covid>();
-        multimap.put(this.getGeold(), covid);
-
-
-    }
-    public void find_highest(MultiMap multiMap) {
-         multiMap = new MultiMap();
-
-
-    }
 
 
     public void read_Covid_Data(String filename){
-        HashMap<String, Integer> mappy = new HashMap<>();
 
         MultiMap<String, Covid> multimap = new MultiMap<String, Covid>();
+
+        Inter it = new Inter();
 
 
         try{
             /**
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the file name: ");
-            System.out.flush();
-            String filename = sc.nextLine();*/
 
+             Scanner sc = new Scanner(System.in);
+             System.out.println("Enter the file name: ");
+             System.out.flush();
+             String filename = sc.nextLine();
+            */
             File file = new File(filename);
 
             FileReader fr = new FileReader(file);
@@ -128,11 +125,18 @@ public class Covid {
 
                     multimap.put(this.getCountry(),new Covid(this.getDateRep(),this.getCountry(),this.getGeold(),this.getNewconfDeaths(),this.getNewConfCases()));
 
-
                 }
-
             }
+            //find_highest_infestion(multimap);
+            //find_second_highest(multimap);
+            //find_death_highest(multimap);
+            Find_country_highest(multimap);
+            int num1 = Overall_deaths(multimap);
+            int num2 = Overall_Infections(multimap);
+            //System.out.println("The Death ratio is "+ (float) num1 / num2  + "\n" +"Or The death ratio is " + num1 + " : "+num2);
 
+            //System.out.println(it.printh());
+            /**
             for(String key : multimap.keySet()) {
                 ArrayList<Covid> covidArrayList = (ArrayList<Covid>) multimap.get(key);
                 for(int i=0; i < covidArrayList.size(); i++){
@@ -142,28 +146,27 @@ public class Covid {
                 highest = 0;
             }
 
-                Collection<Integer> Values = mappy.values();
-                ArrayList<Integer> al = new ArrayList<>();
-                int n =0;
-                for(Integer num : Values){
-                    if(n < num){
-                        n = num;
-                        al.add(n);
-                    }
+            Collection<Integer> Values = mappy.values();
+            ArrayList<Integer> al = new ArrayList<>();
+            int n =0;
+            for(Integer num : Values){
+                if(n < num){
+                    n = num;
+                    al.add(n);
                 }
-                Collections.sort(al);
-                for(Map.Entry m: mappy.entrySet()){
-                    if(m.getValue().equals(n)){
-                        System.out.println("The country with the highest number of infections is " + m.getKey()  + " Num of infections : "+ al.get(al.size() - 1));
+            }
+            Collections.sort(al);
+            for(Map.Entry m: mappy.entrySet()){
+                if(m.getValue().equals(al.get(al.size() - 2))){
+                    System.out.println("The second country with the highest number is "+ m.getKey() + " Num of infections : "+al.get(al.size() - 2));
 
-                    }
-                    else if (m.getValue().equals(al.get(al.size() - 2))){
-                        System.out.println("The second country with the highest number is "+ m.getKey() + " Num of infections : "+al.get(al.size() - 2));
-                    }
                 }
+                else if (m.getValue().equals(n)){
+                    System.out.println("The country with the highest number of infections is " + m.getKey()  + " Num of infections : "+ al.get(al.size() - 1));
+                }
+            }
 
-
-
+             */
             br.close();
 
         } catch (IOException e) {
@@ -173,20 +176,8 @@ public class Covid {
     }
 
 
-    public HashMap<String, Integer> finder(){
-        HashMap<String, Integer> mappy = new HashMap<>();
-        MultiMap<String, Covid> multiMap = new MultiMap<String, Covid>();
-        for(String key : multiMap.keySet()) {
-            ArrayList<Covid> covidArrayList = (ArrayList<Covid>) multiMap.get(key);
-            for(int i=0; i < covidArrayList.size(); i++){
-                highest += covidArrayList.get(i).getNewConfCases();
-                mappy.put(this.getCountry(),highest);
-            }
-            highest = 0;
-        }
-        return mappy;
 
-    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -214,5 +205,177 @@ public class Covid {
                 ", newconfDeaths=" + newconfDeaths +
                 ", newConfCases=" + newConfCases +
                 '}';
+    }
+
+    public void add_covids(MultiMap<String,Covid> multiMap){
+        Covid covid = new Covid();
+        multiMap.put(covid.getCountry(),covid);
+
+    }
+
+
+    public void find_highest_infestion(MultiMap<String,Covid> multiMap){
+        int highest = 0;
+        HashMap<String, Integer> mappy = new HashMap<>();
+        add_covids(multiMap);
+
+        for(String key : multiMap.keySet()) {
+            ArrayList<Covid> covidArrayList = (ArrayList<Covid>) multiMap.get(key);
+            for(int i=0; i < covidArrayList.size(); i++){
+                highest += covidArrayList.get(i).getNewConfCases();
+                mappy.put(key,highest);
+            }
+            highest = 0;
+        }
+
+        Collection<Integer> Values = mappy.values();
+        ArrayList<Integer> al = new ArrayList<>();
+        int n =0;
+        for(Integer num : Values){
+            if(n < num){
+                n = num;
+                al.add(n);
+            }
+        }
+        Collections.sort(al);
+        for(Map.Entry m: mappy.entrySet()){
+            if(m.getValue().equals(n)){
+                System.out.println("The country with the highest number of infections is " + m.getKey()  + " Num of infections : "+ al.get(al.size() - 1));
+            }
+        }
+
+    }
+
+    public void  find_second_highest(MultiMap<String,Covid> multiMap){
+        int highest = 0;
+        HashMap<String, Integer> mappy = new HashMap<>();
+
+        add_covids(multiMap);
+
+        for(String key : multiMap.keySet()) {
+            ArrayList<Covid> covidArrayList = (ArrayList<Covid>) multiMap.get(key);
+            for(int i=0; i < covidArrayList.size(); i++){
+                highest += covidArrayList.get(i).getNewConfCases();
+                mappy.put(key,highest);
+            }
+            highest = 0;
+        }
+
+        Collection<Integer> Values = mappy.values();
+        ArrayList<Integer> al = new ArrayList<>();
+        int n =0;
+        for(Integer num : Values){
+            if(n < num){
+                n = num;
+                al.add(n);
+            }
+        }
+        Collections.sort(al);
+        for(Map.Entry m: mappy.entrySet()){
+            if(m.getValue().equals(al.get(al.size() - 2))){
+                System.out.println("The second country with the highest number is " + m.getKey() + " Num of infections : "+ al.get(al.size() - 2));
+            }
+        }
+
+       // System.out.println(this.getHighest_name());
+    }
+
+
+    public void find_death_highest(MultiMap<String, Covid> multiMap){
+        int highest = 0;
+        HashMap<String, Integer> mappy = new HashMap<>();
+        add_covids(multiMap);
+
+        for(String key : multiMap.keySet()) {
+            ArrayList<Covid> covidArrayList = (ArrayList<Covid>) multiMap.get(key);
+            for(int i=0; i < covidArrayList.size(); i++){
+                highest += covidArrayList.get(i).getNewconfDeaths();
+                mappy.put(key,highest);
+            }
+            highest = 0;
+        }
+
+        Collection<Integer> Values = mappy.values();
+        ArrayList<Integer> al = new ArrayList<>();
+        int n =0;
+        for(Integer num : Values){
+            if(n < num){
+                n = num;
+                al.add(n);
+            }
+        }
+        Collections.sort(al);
+        for(Map.Entry m: mappy.entrySet()){
+            if(m.getValue().equals(n)){
+                System.out.println("The highest death is " + m.getKey()  + " Num of deaths : "+ al.get(al.size() - 1));
+            }
+        }
+    }
+    public String hn(){
+        return country;
+    }
+
+    public int  Overall_deaths(MultiMap<String,Covid> multiMap){
+        int highest = 0;
+        HashMap<String, Integer> mappy = new HashMap<>();
+
+        add_covids(multiMap);
+
+        for(String key : multiMap.keySet()) {
+            ArrayList<Covid> covidArrayList = (ArrayList<Covid>) multiMap.get(key);
+            for(int i=0; i < covidArrayList.size(); i++){
+                highest += covidArrayList.get(i).getNewconfDeaths();
+            }
+        }
+        return highest;
+    }
+    public int  Overall_Infections(MultiMap<String,Covid> multiMap){
+        int highest = 0;
+        HashMap<String, Integer> mappy = new HashMap<>();
+
+        add_covids(multiMap);
+
+        for(String key : multiMap.keySet()) {
+            ArrayList<Covid> covidArrayList = (ArrayList<Covid>) multiMap.get(key);
+            for(int i=0; i < covidArrayList.size(); i++){
+                highest += covidArrayList.get(i).getNewConfCases();
+            }
+        }
+        return highest;
+    }
+
+    public String Find_country_highest(MultiMap<String,Covid> multiMap){
+        int highest = 0;
+        String name = null;
+        HashMap<String, Integer> mappy = new HashMap<>();
+        add_covids(multiMap);
+
+        for(String key : multiMap.keySet()) {
+            ArrayList<Covid> covidArrayList = (ArrayList<Covid>) multiMap.get(key);
+            for(int i=0; i < covidArrayList.size(); i++){
+                highest += covidArrayList.get(i).getNewConfCases();
+                mappy.put(key,highest);
+            }
+            highest = 0;
+        }
+
+        Collection<Integer> Values = mappy.values();
+        ArrayList<Integer> al = new ArrayList<>();
+        int n =0;
+        for(Integer num : Values){
+            if(n < num){
+                n = num;
+                al.add(n);
+            }
+        }
+        Collections.sort(al);
+        for(Map.Entry m: mappy.entrySet()){
+            if(m.getValue().equals(n)){
+                System.out.println("The country with the highest number of infections is " + m.getKey()  + " Num of infections : "+ al.get(al.size() - 1));
+                name += String.valueOf(m.getKey());
+                return name;
+            }
+        }
+        return name;
     }
 }
